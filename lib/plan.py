3b927e827eb76279d113e58e43b6a630f1918782
Mikita Charnysh <mr.nick.cher@gmail.com>
2018-06-06 20:15:23 +0300

    periodic plans added

---
+++ lib/plan.py
@@ -0,0 +1,66 @@
+import datetime as dt
+import copy
+from lib.task import TaskAttributes
+from uuid import uuid1
+
+
+class PeriodicPlanAttributes:
+    PERIOD = 'period'
+    END_DATE = 'end_date'
+    TASK_ID = 'task_id'
+    UID = 'uid'
+    TASK_TEMPLATE = 'task_template'
+    USER = 'user'
+    LAST_UPDATE_TIME = 'last_update_time'
+
+class PeriodicPlan:
+    def __init__(self, period, task_template, task_id, user, uid=None, end_date=None, last_update_time = None):
+        self.period = period
+        self.task_template = task_template
+        self.task_id = task_id
+        self.user = user
+        if uid is not None:
+            self.uid = uid
+        else:
+            self.uid = uuid1()
+        self.end_date = end_date
+        self.last_update_time = last_update_time
+
+    def get_next_periodic_task(self):
+        start_date = self.task_template.try_get_attribute(TaskAttributes.START_DATE)
+        remind_dates = self.task_template.try_get_attribute(TaskAttributes.REMIND_DATES)
+        end_date = self.task_template.try_get_attribute(TaskAttributes.END_DATE)
+
+        new_task = copy.copy(self.task_template)
+
+        start_date += self.period
+        end_date += self.period
+
+        if remind_dates is not None:
+            for i in range(len(remind_dates)):
+                remind_dates[i] += self.period
+            new_task.set_attribute(TaskAttributes.REMIND_DATES, remind_dates, self.user)
+
+        new_task.set_attribute(TaskAttributes.START_DATE,start_date,self.user)
+        new_task.set_attribute(TaskAttributes.END_DATE,end_date,self.user)
+        new_task.attributes[TaskAttributes.UID] = uuid1()
+        self.task_template = copy.copy(new_task)
+        self.last_update_time = start_date
+
+        return new_task
+
+    def periodic_update_needed(self):
+        if self.last_update_time is None:
+            return True
+        delta = dt.datetime.now() - self.last_update_time
+        if delta >= self.period:
+            return True
+        return False
+
+
+
+
+
+
+
+
---
+++ lib/plans_manager.py
@@ -0,0 +1,27 @@
+
+
+class PlansManager:
+    def __init__(self, plans_dict=None):
+        if plans_dict is None:
+            self.plans = {}
+        else:
+            self.plans = plans_dict
+
+    def add_plan(self, plan):
+        self.plans[plan.uid] = plan
+
+    def remove_plan(self,plan_id):
+        self.plans.pop(plan_id)
+
+    def try_find_plan_for_task(self, task_id):
+        for k,v in self.plans.items():
+            if v.task_id == task_id:
+                return k
+        return None
+
+    def get_updates(self):
+        tasks_to_add = []
+        for k,v in self.plans.items():
+            if v.periodic_update_needed():
+                tasks_to_add.append(v.get_next_periodic_task())
+        return tasks_to_add
