efbcbe7617aaccea590eab06bfbdc68c132753dd
Mikita Charnysh <mr.nick.cher@gmail.com>
2018-06-07 23:02:50 +0300

    global directory and files path

--- json_db/default_config.py
+++ json_db/default_config.py
@@ -1,2 +1,5 @@
-DEFAULT_TASKS_FILE = "../tasks.json"
-DEFAULT_PLANS_FILE = "../plans.json"
+import os
+
+DEFAULT_STORAGE_DIR = os.path.join(os.environ['HOME'], 'py_tracker')
+DEFAULT_TASKS_FILE = os.path.join(DEFAULT_STORAGE_DIR, "tasks.json")
+DEFAULT_PLANS_FILE = os.path.join(DEFAULT_STORAGE_DIR, "plans.json")
