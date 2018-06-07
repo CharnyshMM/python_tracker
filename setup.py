2ee6d5689fd647532daad12413082bf95948cd8d
Mikita Charnysh <mr.nick.cher@gmail.com>
2018-06-07 20:01:58 +0300

    setup.py for entire app created

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python_tracker',
+    version='0.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts':
+            ['py_tracker = console_interface.main:main']
+    },
+    url='',
+    license='',
+    author='mikita',
+    author_email='',
+    description='task tracker library and console interface for it'
+)
