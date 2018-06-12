0b9bce2268e7e57dc7895c17c715cb2311191c7a
Mikita Charnysh <mr.nick.cher@gmail.com>
2018-06-12 11:08:39 +0300

    plan manager test added

--- lib/tests/plans_manager_tests.py
+++ lib/tests/plans_manager_tests.py
@@ -1,47 +1,69 @@
-from lib.entities.plan import PeriodicPlan
+from lib.entities.plan import PeriodicPlan,Period
+from lib.entities.plans_manager import PlansManager
 from lib.entities.task import Task, TaskAttributes
 import datetime as dt
 import unittest
 
 
 class PlanTestCase(unittest.TestCase):
-    PERIOD = dt.timedelta(minutes=3)
-    START_DATE = dt.datetime.now()
-    TIMEDELTA = dt.timedelta(minutes=5)
 
     TITLE = 'title'
     AUTHOR = 'tester'
     USER_1 = 'user_1'
+    MINUTES_DELTA = dt.timedelta(minutes=10)
+    DAY_DELTA = dt.timedelta(days=1)
 
     def setUp(self):
-        self.task = Task(title=self.TITLE,
-                         author=self.AUTHOR,
-                         start_time=self.START_DATE,
-                         end_time=self.START_DATE + self.TIMEDELTA,
-                         can_edit=[self.USER_1]
-                         )
-        task_id = self.task.get_attribute(TaskAttributes.UID)
-        self.plan_ = PeriodicPlan(period=self.PERIOD,
-                                 task_template=self.task,
-                                 task_id=task_id,
-                                 user=self.USER_1)
+        self.plans_manager = PlansManager()
+        self.minutes_task_start_time = start_time = dt.datetime.now()
+        self.minutes_task = Task(self.TITLE+"min", self.AUTHOR, start_time=self.minutes_task_start_time)
+        self.minutes_task_id = self.minutes_task.get_attribute(TaskAttributes.UID)
+        self.minutes_plan = PeriodicPlan(self.MINUTES_DELTA, self.minutes_task, self.minutes_task_id, self.AUTHOR)
+        self.plans_manager.add_plan(self.minutes_plan)
+
+        self.day_task_start_time = start_time=dt.datetime.now() - 4 * self.DAY_DELTA
+        self.day_task = Task(self.TITLE+"day", self.AUTHOR, start_time=self.day_task_start_time)
+        self.day_task_id = self.day_task.get_attribute(TaskAttributes.UID)
+        self.day_plan = PeriodicPlan(self.DAY_DELTA, self.day_task, self.day_task_id, self.AUTHOR,
+                                     end_time=self.day_task_start_time + 2*self.DAY_DELTA)
+        self.plans_manager.add_plan(self.day_plan)
 
     def tearDown(self):
-        self.task = None
-        self.plan = None
+        self.plans_manager = None
+        self.day_task = None
+        self.day_task_id = None
+        self.day_plan = None
+        self.minutes_plan = None
+        self.minutes_task_id = None
+        self.minutes_task = None
 
-    def test_plan_update(self):
-        self.plan.last_update_time = None
-        self.assertTrue(self.plan.periodic_update_needed())
-        next_task = self.plan.get_next_periodic_task()
-        self.assertEqual(next_task.get_attribute(TaskAttributes.START_TIME),
-                         self.task.get_attribute(TaskAttributes.START_TIME) + self.PERIOD)
-        self.assertEqual(next_task.get_attribute(TaskAttributes.PLAN), self.plan.uid)
+    def test_get_updates(self):
+        updates = self.plans_manager.get_updates()
 
-    def test_plan_stop(self):
-        if self.plan.periodic_update_needed():
-            self.plan.get_next_periodic_task()
-        self.assertFalse(self.plan.periodic_update_needed())
+        day_forward = Period.add_timedelta(self.DAY_DELTA, self.day_task_start_time)
+        two_days_forward = Period.add_timedelta(self.DAY_DELTA, day_forward)
+
+        minutes_forward = Period.add_timedelta(self.MINUTES_DELTA,self.minutes_task_start_time)
+
+        minutes_task_correct = False
+        day_1_task_correct = False
+        day_2_task_correct = False
+        unknown = False
+        for task in updates:
+            if task.get_attribute(TaskAttributes.START_TIME) == minutes_forward:
+                minutes_task_correct = True
+            elif task.get_attribute(TaskAttributes.START_TIME) == day_forward:
+                day_1_task_correct = True
+            elif task.get_attribute(TaskAttributes.START_TIME) == two_days_forward:
+                day_2_task_correct = True
+            else:
+                unknown = True
+        self.assertFalse(unknown)
+        self.assertTrue(minutes_task_correct and day_1_task_correct and day_2_task_correct)
+
+
+
+
 
 
 if __name__ == '__main__':
