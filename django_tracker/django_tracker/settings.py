c596d6c6324814794990c012b03103dba925ddcd
Mikita Charnysh <mr.nick.cher@gmail.com>
2018-06-21 23:50:08 +0300

    login redirect const added

--- django_tracker/django_tracker/settings.py
+++ django_tracker/django_tracker/settings.py
@@ -114,6 +114,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Authentication consts
+
+LOGIN_REDIRECT_URL = '/'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
