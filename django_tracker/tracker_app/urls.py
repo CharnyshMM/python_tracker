fb19c106d37fb6acfa4f4a6ab9842375bf458b6b
Mikita Charnysh <mr.nick.cher@gmail.com>
2018-06-24 00:56:14 +0300

    renamed task/ to tasks/, plans pages urls added

--- django_tracker/tracker_app/urls.py
+++ django_tracker/tracker_app/urls.py
@@ -12,13 +12,16 @@ urlpatterns = [
     url(r'^accounts/logout/$', auth_views.logout,{'template_name': 'tracker_app/logout.html'}, name='logout'),
     url(r'^accounts/signup/$', views.signup, name='signup'),
     # /tracker_app/task/5
-    url(r'^task/(?P<task_id>\d+)/$', view=views.detail, name='detail_task'),
+    url(r'^tasks/(?P<object_id>\d+)/$', view=views.detail, name='detail_task'),
     # /tracker_app/task/new
-    url(r'^task/(?P<task_id>\d+)/new/$', view=views.new_task, name='new_task'),
+    url(r'^tasks/(?P<object_id>\d+)/new/$', view=views.new_task, name='new_task'),
     # /tracker_app/task/5/edit
-    url(r'^task/(?P<task_id>\d+)/edit/$', view=views.edit_task, name='edit_task'),
-    url(r'^task/(?P<task_id>\d+)/delete/$', view=views.delete_task, name='delete_task'),
-    url(r'^task/(?P<task_id>\d+)/new_plan/$',view=views.add_plan, name='new_plan'),
+    url(r'^tasks/(?P<object_id>\d+)/edit/$', view=views.edit_task, name='edit_task'),
+    url(r'^tasks/(?P<object_id>\d+)/delete/$', view=views.delete_task, name='delete_task'),
+    url(r'^tasks/(?P<object_id>\d+)/new_plan/$',view=views.add_plan, name='new_plan'),
+    url(r'^plans/(?P<object_id>\d+)/delete/$',view=views.delete_plan, name='delete_plan'),
+    # /tracker_app/plans
+    url(r'^plans/', view=views.all_plans, name='all_plans'),
     # /tracker_app/thanks
     url(r'^thanks/$', view=views.thanks, name='thanks'),
 ]
